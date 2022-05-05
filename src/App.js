02bead3b28b2b6a24ae010af0f09fec93d418b9f
abrao <arnf2710@gmail.com>
2022-05-05 15:18:19 -0300

    First commit defining the starting project

---
+++ src/App.js
@@ -0,0 +1,7 @@
+const App = () => (
+  <div className="h-screen flex items-center justify-center w-full">
+    <h1 className="text-5xl text-sky-400 font-bold border-b-4 border-sky-400 pb-3">Starting project</h1>
+  </div>
+);
+
+export default App;
