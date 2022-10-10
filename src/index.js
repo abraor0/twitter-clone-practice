661f64370771974b708ed2a237210dab030cdc12
Abrão Rodrigues <arnf2710@gmail.com>
2022-10-10 09:04:19 -0300

    Adjusted router base path

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ import AuthProvider from './store/auth-context';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename='/twitter-clone-practice'>
       <AuthProvider>
         <NotificationProvider>
           <App />
