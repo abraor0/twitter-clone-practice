4dc5d10c0aaa3a2c854cbbdab1b1d4ba059e3908
Abrão Rodrigues <arnf2710@gmail.com>
2023-03-01 19:52:34 -0300

    Adds remaining types for the App component

--- src/components/Notification/Notification.tsx
+++ src/components/Notification/Notification.tsx
@@ -1,4 +1,8 @@
-const Notification = ({ message }) => {
+interface NotificationProps {
+  message: string;
+}
+
+const Notification = ({ message }: NotificationProps) => {
   return (
     <span className="text-sm px-8 py-4 bg-sky-500 text-white fixed bottom-5 rounded-md left-1/2 -translate-x-1/2 z-20">
       {message}
