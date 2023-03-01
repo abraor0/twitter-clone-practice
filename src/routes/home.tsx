5946919605cd34fb376ab5e5523f4adc4211ae15
Abrão Rodrigues <arnf2710@gmail.com>
2023-03-01 19:57:48 -0300

    Fixes loop bug caused by useEffect

--- src/routes/home.tsx
+++ src/routes/home.tsx
@@ -1,5 +1,5 @@
 import { useEffect } from 'react';
-import { fetchPosts } from '../apis/firestore';
+import { fetchPosts, TweetTypes } from '../apis/firestore';
 import Tweet from '../components/Tweet/Tweet';
 import TweetArea from '../components/TweetArea/TweetArea';
 import MainContentHeader from '../components/UI/MainContentHeader';
@@ -8,23 +8,25 @@ import Spinner from '../components/UI/Spinner';
 import { useNotificationContext } from '../store/notification-context';
 
 const Home = () => {
-  const { data, error, status, sendRequest } = useHttp(fetchPosts, []);
+  const { data, error, status, sendRequest } = useHttp(fetchPosts);
   const { setMessage } = useNotificationContext();
 
   useEffect(() => {
     sendRequest();
-  }, []);
+  }, [ ]);
 
   useEffect(() => {
-    if (status === 'error') setMessage(error!);
-  }, [status]);
+    if (status === 'error' && error) setMessage(error);
+  }, [status, error]);
+
 
   return (
     <section>
       <MainContentHeader contentTitle={'Home'} />
       <TweetArea />
       {data &&
-        data.map((tweet, index) => <Tweet key={index} {...tweet} />)}
+        // TODO: Remove assertion
+        data.map((tweet, index) => <Tweet key={index} {...(tweet as TweetTypes)} />)}
       {status === 'pending' && <div className="flex mt-8 justify-center"><Spinner /></div>}
     </section>
   );
